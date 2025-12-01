220ad683e7835e9b7a7e0867c89fac0e3e780866
mohitsankhyan81 <mohitsankhyan253@gmail.com>
2025-12-01 15:16:45 +0530

    commit

--- client/src/components/Trailer.jsx
+++ client/src/components/Trailer.jsx
@@ -12,13 +12,17 @@ const Trailer = () => {
       <p className='text-gray-300 font-medium text-lg max-w-[960px] mx-auto'>Trailer</p>
       <div className='relative mt-6 bg-gray-900 p-4 rounded-lg'>
         <BlurCircle top='-100px' right='-100px'/>
+<div className='w-full flex justify-center bg-gray-800'>
 <ReactPlayer
-  url={currentTrailer.videoUrl}
-  controls={false}
-  className='mx-auto max-w-full'
-  width="960px"
-  height="540px"
+  url="https://www.youtube.com/watch?v=ysz5S6PUM-U"
+  controls
+  playing
+  width="100%"
+  height="480px"
 />
+
+</div>
+
 
       </div>
     </div>
